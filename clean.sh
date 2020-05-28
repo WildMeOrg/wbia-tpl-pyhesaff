7feee8bd26e9b6d889b2bf16550905d0e9fc2784
Jason Parham <bluemellophone@gmail.com>
2020-05-28 12:14:44 -0700

    Properly link libraries to OpenCV

--- clean.sh
+++ clean.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 rm -rf _skbuild
 rm -rf pyhesaff/lib
 rm -rf dist
@@ -10,5 +9,5 @@ rm -rf pyhesaff.egg-info
 rm -rf mb_work
 rm -rf wheelhouse
 
-CLEAN_PYTHON='find . -regex ".*\(__pycache__\|\.py[co]\)" -delete || find . -iname *.pyc -delete || find . -iname *.pyo -delete'
+CLEAN_PYTHON='find . -iname __pycache__ -delete || find . -iname *.pyc -delete || find . -iname *.pyo -delete'
 bash -c "$CLEAN_PYTHON"
