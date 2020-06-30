115df94e16790a140b8158acbbf03f13b5f5fbe2
Michael Mulich <michael.mulich@gmail.com>
2020-06-30 10:46:58 -0700

    pre-commit fix-encoding-pragma

--- dev/_grave/docker_build.py
+++ dev/_grave/docker_build.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """
 References:
     https://github.com/skvark/opencv-python
--- docker/build_opencv_docker.py
+++ docker/build_opencv_docker.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """
 Setup the base image containing the opencv deps that pyhesaff needs to build
 
